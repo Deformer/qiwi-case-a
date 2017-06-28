a16e142d7176783b1a831f8b38b771a1ee8b61ba
sergey <serg19-03@mail.ru>
2017-06-28 11:10:35 +0300

    add eslint and ignore .idea/

--- routes/api/dialog.js
+++ routes/api/dialog.js
@@ -2,19 +2,19 @@ const router = require('express').Router();
 const dialogService = require('../../services/dialog');
 const messageService = require('../../services/message');
 
-router.get('/',(req,res) => {
-  dialogService.getAllDialogsToUser(req.user.id).then(dialogs => {
+router.get('/', (req, res) => {
+  dialogService.getAllDialogsToUser(req.user.id).then((dialogs) => {
     res.status(200).send(dialogs);
-  })
+  });
 });
-router.post('/', (req,res) => {
-  const {members} = req.body;
+router.post('/', (req, res) => {
+  const { members } = req.body;
   dialogService.createDialog(members).then((users) => {
     res.status(200).send();
-  })
+  });
 });
 
-/*{
+/* {
   "message":{
   "type": "online",
     "money": 100,
@@ -23,9 +23,9 @@ router.post('/', (req,res) => {
     "dialogId": 6
 }
 }*/
-router.post('/postMessage',(req,res) => {
-  const {user} = req;
-  const {message} = req.body;
+router.post('/postMessage', (req, res) => {
+  const { user } = req;
+  const { message } = req.body;
   message.from = user.id;
   messageService.saveMessage(message).then((response) => {
     res.sendStatus(200);

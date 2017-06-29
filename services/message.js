1eeb02a6e0a22821081e84833c08f4077efd85bf
akvone <illusspace@gmail.com>
2017-06-29 18:46:04 +0300

    Изменения socketEvents

--- services/message.js
+++ services/message.js
@@ -3,27 +3,33 @@ const userService = require('./user');
 const dialogService = require('./dialog');
 
 module.exports = {
-  getById: (id) => Message.findOne({where:{id}}),
-  saveMessage: messageObject => new Promise((resolve, reject) => {
-    userService.checkIfUserExistWithId(messageObject.from).then((isFromExist) => {
-      if (!isFromExist) {
-        return resolve({code: 404, message: 'from is not exist'});
-      }
-      userService.checkIfUserExistWithId(messageObject.to).then((isToExist) => {
-        if (!isToExist) {
-          return resolve({code: 404, message: 'to is not exist'});
-        }
-        dialogService.checkIfDialogExistWithId(messageObject.dialogId).then((isDialogExist) => {
-          if (!isDialogExist) {
-            return resolve({code: 404, message: 'dialog is not exist'});
-          }
-          const message = Message.build(messageObject);
-          message.save().then(() => {
-            resolve(message);
-          });
+    getById: (id) => Message.findOne({where: {id}}),
+    saveMessage: messageObject => new Promise((resolve, reject) => {
+        userService.checkIfUserExistWithId(messageObject.from).then((isFromExist) => {
+            if (!isFromExist) {
+                return resolve({code: 404, message: 'from is not exist'});
+            }
+            userService.checkIfUserExistWithId(messageObject.to).then((isToExist) => {
+                if (!isToExist) {
+                    return resolve({code: 404, message: 'to is not exist'});
+                }
+                dialogService.checkIfDialogExistWithId(messageObject.dialogId).then((isDialogExist) => {
+                    if (!isDialogExist) {
+                        return resolve({code: 404, message: 'dialog is not exist'});
+                    }
+                    const message = Message.build(messageObject);
+                    message.save().then(() => {
+                        resolve(message);
+                    });
+                });
+            });
         });
-      });
-    });
-  }),
-  confirmMessage: (userId, messageId) => Message.update({isConfirmed: true}, {where: {id: messageId, to: userId, isConfirmed: false}})
+    }),
+    confirmMessage: (userId, messageId) => Message.update({isConfirmed: true}, {
+        where: {
+            id: messageId,
+            to: userId,
+            isConfirmed: false
+        }
+    })
 };
--- socketEvents.js
+++ socketEvents.js
@@ -6,71 +6,96 @@ const balanceService = require('./services/balance');
 
 const openedConnections = [];
 
+let confirmMessageAndUpdateBalance = (message) => {
+    messageService.confirmMessage(message).then((result) => {
+        if (result[0] > 0) {
+            balanceService.changeBalance(message.money, message.from, message.dialogId).then(() => {
+                balanceService.changeBalance(-1 * message.money, message.to, message.dialogId).then(() => {
+                    return true;
+                })
+            })
+        }
+    })
+};
+
+let answerOnMessage = function (socket, message) {
+    let messageToSender = {
+        messageId: message.id,
+    };
+    let messageToRecipient = {
+        messageId: message.id,
+    };
+    balanceService.getBalance(message.from, message.dialogId).then((senderBalance) => {
+        balanceService.getBalance(message.to, message.dialogId).then((recipientBalance) => {
+            messageToSender.balance = senderBalance;
+            messageToRecipient.balance = recipientBalance;
+            socket.emit('responseConfirmMessage', messageToRecipient);
+            if (openedConnections[message.from]) {
+                openedConnections[message.from].emit('responseConfirmMessage', messageToSender);
+            }
+        })
+    })
+};
+
 module.exports = {
     init: function (io) {
         io.sockets
-            .on('connection', socketioJwt.authorize({
-                secret: config.secret,
-                timeout: 15000, // 15 seconds to send the authentication message
-            }))
-            .on('authenticated', (socket) => {
-                openedConnections[socket.decoded_token.id] = socket;
+          .on('connection', socketioJwt.authorize({
+              secret: config.secret,
+              timeout: 15000, // 15 seconds to send the authentication message
+          }))
+          .on('authenticated', (socket) => {
+              openedConnections[socket.decoded_token.id] = socket;
 
-                socket.on('disconnect', () => {
-                    console.log(`User with id = ${socket.decoded_token.id} has been disconnected`);
-                    delete openedConnections[socket.decoded_token.id];
-                });
+              socket.on('disconnect', () => {
+                  console.log(`User with id = ${socket.decoded_token.id} has been disconnected`);
+                  delete openedConnections[socket.decoded_token.id];
+              });
 
-                socket.on('sendMessage', ({to, money, dialogId, comment, type}) => {
-                    const userId = socket.decoded_token.id;
-                    const message = {to, money, dialogId, comment, type};
-                    message.from = userId;
-                    console.log(message);
-                    if (message.type === 'online') {
-                        message.isConfirmed = true;
-                        messageService.saveMessage(message).then((response) => {
-                            balanceService.changeBalance(message.money, message.from, message.dialogId).then(() => {
-                                balanceService.changeBalance(-1 * message.money, message.to, message.dialogId).then((messageFromDb) => {
-                                    balanceService.getBalance(message.from, message.dialogId);
-                                    socket.emit('message', response.dataValues);
-                                    if (openedConnections[message.to]) {
-                                        openedConnections[message.to].emit('message', response.dataValues);
-                                    }
-                                })
-                            })
-                        });
-                    } else if (message.type === 'cash') {
-                        message.isConfirmed = false;
-                        messageService.saveMessage(message).then((response) => {
-                            socket.emit('message', response.dataValues);
-                            if (openedConnections[message.to]) {
-                                openedConnections[message.to].emit('message', response.dataValues);
-                            }
-                        })
-                    }
-                });
-                socket.on('requestConfirmMessage',
-                    ({
-                         messageId,
-                         answer
-                     }) => {
-                        console.log(`Get confirm request ${messageId}/${answer}`);
-                        messageService.getById(messageId).then(message => {
-                            if (message.to === socket.decoded_token.id) {
-                                messageService.confirmMessage(socket.decoded_token.id, messageId).then();
-                                socket.emit('responseConfirmMessage', "Молодец");
-                                if (openedConnections[message.from]) {
-                                    openedConnections[message.from].emit('responseConfirmMessage', "И ты тоже молодец");
-                                }
-                            }
-                            else {
-                                socket.emit('responseConfirmMessage', "Пшел нахуй");
-                                if (openedConnections[message.from]) {
-                                    openedConnections[message.from].emit('responseConfirmMessage', "И ты тоже пшел нахуй");
-                                }
-                            }
-                        });
-                    });
-            });
+              socket.on('sendMessage', ({to, money, dialogId, comment, type}) => {
+                  const userId = socket.decoded_token.id;
+                  const message = {to, money, dialogId, comment, type};
+                  message.from = userId;
+                  console.log(message);
+                  if (message.type === 'online') {
+                      message.isConfirmed = true;
+
+                      //Todo немножко нехорошо, потому что 2 запроса в БД подряд.
+                      messageService.saveMessage(message).then((response) => {
+                          confirmMessageAndUpdateBalance(message).then(response => {
+                              if (response === true) {
+                                  answerOnMessage(socket, message);
+                              }
+                          });
+                      });
+                  } else if (message.type === 'cash') {
+                      message.isConfirmed = false;
+                      messageService.saveMessage(message).then((response) => {
+                          socket.emit('message', response.dataValues);
+                          if (openedConnections[message.to]) {
+                              openedConnections[message.to].emit('message', response.dataValues);
+                          }
+                      })
+                  }
+              });
+              socket.on('requestConfirmMessage', ({messageId, answer}) => {
+                  console.log(`Get confirm request ${messageId}/${answer}`);
+                  messageService.getById(messageId).then(message => {
+                      if (message.to === socket.decoded_token.id) {
+                          confirmMessageAndUpdateBalance(message).then(response => {
+                              if (response === true) {
+                                  answerOnMessage(socket, message);
+                              }
+                          })
+                      }
+                      // else {
+                      //     socket.emit('responseConfirmMessage', "Пшел нахуй");
+                      //     if (openedConnections[message.from]) {
+                      //         openedConnections[message.from].emit('responseConfirmMessage', "И ты тоже пшел нахуй");
+                      //     }
+                      // }
+                  });
+              })
+          })
     }
 };
